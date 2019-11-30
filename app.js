35504afcb04675a07e7de478cbd7fb9ca165c656
Safouene Chemkhi <safouene.cs@gmail.com>
2019-11-30 23:47:13 +0100

    Connecting MongoDB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // connecting to the mongodb
-mongoose.connect('mongodb://localhost/hexoskinApp', {
+mongoose.connect('mongodb://localhost/remote_united', {
   useCreateIndex: true,
   useNewUrlParser: true
 })
